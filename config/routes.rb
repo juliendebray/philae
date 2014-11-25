336804c55665691ddd4b482575e5e157854d0e0d
Julien Debray <julien.debray.pro@gmail.com>
2014-11-25 11:40:47 +0100

    route from hp to access index of experiences created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root 'home#index'
+
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
+  resources :experiences
+
+end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -57,4 +62,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
