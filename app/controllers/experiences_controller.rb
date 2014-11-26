5229a36c07edc7192e5ccb7c79fb0ebd58c9e270
Julien Debray <julien.debray.pro@gmail.com>
2014-11-26 19:59:48 +0100

    render json to the view with the markers to display

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -4,18 +4,26 @@ class ExperiencesController < ApplicationController
   # GET /experiences
   # GET /experiences.json
   def index
-    @experiences = Experience.all
-    @markers = Gmaps4rails.build_markers(@experiences) do |experience, marker|
+    # @experiences = Experience.all
+    # @markers = Gmaps4rails.build_markers(@experiences) do |experience, marker|
+    #   marker.lat experience.latitude
+    #   marker.lng experience.longitude
+    #   marker.infowindow render_to_string(partial: "/experiences/infowindow.html.erb", locals: { experience: experience})
+    #   marker.title experience.name
+    # end
+  end
+
+  def markers
+    @experiences = Experience.within_bounding_box([params[:SWLA].to_f, params[:SWLO].to_f, params[:NELA].to_f, params[:NELO]])
+    @experiences.sort_by{ |e| e.experience_reviews.average(:rating) }
+    @markers = Gmaps4rails.build_markers(@experiences.reverse[0...2]) do |experience, marker|
       marker.lat experience.latitude
       marker.lng experience.longitude
       marker.infowindow render_to_string(partial: "/experiences/infowindow.html.erb", locals: { experience: experience})
       marker.title experience.name
     end
-  end
-
-  def markers
-    @markers = {}
     render json: @markers
+    raise
   end
 
   # GET /experiences/1
