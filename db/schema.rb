8ec3ca3dbe72ea386dbc83ff6fe46459a2eb09f7
Julien Debray <julien.debray.pro@gmail.com>
2014-11-30 17:34:56 +0100

    add query column to trip model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129132952) do
+ActiveRecord::Schema.define(version: 20141130163350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 20141129132952) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "query"
   end
 
   add_index "trips", ["user_id"], name: "index_trips_on_user_id", using: :btree
