049e1915026688dc86d4c20e9b6dadf353d38df3
Julien Debray <julien.debray.pro@gmail.com>
2014-12-03 19:56:29 +0100

    list and destroy comment on the show view

--- app/controllers/trip_comments_controller.rb
+++ app/controllers/trip_comments_controller.rb
@@ -1,5 +1,21 @@
 class TripCommentsController < ApplicationController
+  before_action :set_trip, only: [:create]
+
   def create
-    raise
+    @trip_comment = @trip.trip_comments.create(trip_comments_params)
+  end
+
+  def destroy
+    @trip_comment = TripComment.find(params[:id])
+    @trip_comment.destroy
+  end
+
+  private
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
+
+  def trip_comments_params
+    params.require(:trip_comment).permit(:name, :address, :description)
   end
 end
