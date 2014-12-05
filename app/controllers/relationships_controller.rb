f9d738b3783cfc555811baa0ea0d2f180ff611b8
Julien Debray <julien.debray.pro@gmail.com>
2014-12-05 11:01:14 +0100

    create relationship

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,23 @@
 class RelationshipsController < ApplicationController
+  def create
+    if friend = User.find_by(email: params[:email])
+      current_user.relationships.create(
+        friend_id: friend.id,
+        sender_id: current_user.id,
+        )
+      friend.relationships.create(
+        friend_id: current_user.id,
+        sender_id: current_user.id,
+        )
+    else
+      flash[:notice] = 'no such email found in our database'
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def relationship_params
+  end
+
 end
