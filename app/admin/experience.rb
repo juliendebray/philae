3a08adaed37f10efd6c6abb2efd57485f68d8d5b
Julien Debray <julien.debray.pro@gmail.com>
2014-12-21 19:12:02 +0100

    active admin implemented

---
+++ app/admin/experience.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register Experience do
+
+  permit_params :name, :address, :description, :published, :user_id, :private, :published_at, :created_at, :updated_at, :average_rating # etc...
+
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
