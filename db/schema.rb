739b0805a9bdedccd9ae53687c96a5ec0f45d259
Julien Debray <julien.debray.pro@gmail.com>
2014-12-21 18:33:07 +0100

    attempt to implement active admin within the user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219123234) do
+ActiveRecord::Schema.define(version: 20141221111652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -155,6 +170,7 @@ ActiveRecord::Schema.define(version: 20141219123234) do
     t.string   "name"
     t.string   "token"
     t.datetime "token_expiry"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
