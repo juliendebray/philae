de766475ab8f947d0210ca3cd1d5dfa1b83c11a2
Julien Debray <julien.debray.pro@gmail.com>
2015-01-07 08:54:27 +0100

    enable socio-demo data in google analytics reports

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -4,4 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-58279830-1', 'explorizers.com');
+ga('require', 'displayfeatures');
 ga('send', 'pageview');
