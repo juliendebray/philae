5bf5f535be1e4f4d2af36e600f5c29bb3804e12d
Julien Debray <julien.debray.pro@gmail.com>
2015-01-15 19:34:08 +0100

    add active admin for service and provider

---
+++ app/admin/service.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Service do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :experience_id, :porvider_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
