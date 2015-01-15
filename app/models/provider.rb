fdaa756fa25931ef9c80204f1d6b85288d198923
Julien Debray <julien.debray.pro@gmail.com>
2015-01-15 12:25:22 +0100

    add picture with paperclip on provider model

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,13 @@
 class Provider < ActiveRecord::Base
   has_many :services
   has_many :experiences, through: :services
+
+  has_attached_file :picture,
+      styles: {
+        medium: "300x300#",
+        thumb: "100x100#"
+      }
+
+    validates_attachment_content_type :picture,
+      content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115111257) do
+ActiveRecord::Schema.define(version: 20150115111837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,10 @@ ActiveRecord::Schema.define(version: 20150115111257) do
     t.string   "phone"
     t.string   "website"
     t.text     "activities_description"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "relationships", force: true do |t|
