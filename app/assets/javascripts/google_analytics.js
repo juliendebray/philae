df1d3aea9d29bb44de325d3629b1336bacf7c757
Julien Debray <julien.debray.pro@gmail.com>
2015-01-21 18:10:04 +0100

    configure event for ga

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -3,6 +3,6 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-58279830-1', 'explorizers.com');
+ga('create', 'UA-58279830-1', 'auto');
 ga('require', 'displayfeatures');
 ga('send', 'pageview');
