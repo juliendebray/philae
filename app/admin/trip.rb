9d520217e52a6c8eee715c671246479270997650
Julien Debray <julien.debray.pro@gmail.com>
2015-02-02 10:31:18 +0100

    add trip to active admin

---
+++ app/admin/trip.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Trip do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :trip_id, :title, :user_id, :query, :latitude, :longitude
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
