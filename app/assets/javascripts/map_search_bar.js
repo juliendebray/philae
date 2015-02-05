c7d6060d0e4efa5c6148ba8890030367865db939
Julien Debray <julien.debray.pro@gmail.com>
2015-02-05 16:16:06 +0100

    add search bar on map show

---
+++ app/assets/javascripts/map_search_bar.js
@@ -0,0 +1,43 @@
+function initializeMapSearchBar(id) {
+
+  var input = document.getElementById(id);
+  var defaultBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(-90, 180),
+      new google.maps.LatLng(90, 180)
+  );
+
+  var options = {
+      bounds: defaultBounds,
+      types: ['geocode']
+  };
+
+  var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+  var map = handler.getMap();
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var place =  this.getPlace();
+    if(typeof place.geometry !== "undefined") {
+      moveMapAccordinglyToViewport(place, map);
+    } else if (place.name !== "undefined") {
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode( { 'address': place.name}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          moveMapAccordinglyToViewport(results[0], map);
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
+    } else {
+        alert('Something went wrong :(');
+    }
+  });
+}
+
+function moveMapAccordinglyToViewport(result, map) {
+  if (result.geometry.viewport) {
+    map.fitBounds(result.geometry.viewport);
+  } else {
+    map.setCenter(result.geometry.location);
+  }
+}
