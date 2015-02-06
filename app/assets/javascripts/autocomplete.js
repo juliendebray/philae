e7a36561fb70a05f9a5ab72bb43fe9b70fe0bfba
Julien Debray <julien.debray.pro@gmail.com>
2015-02-06 17:39:42 +0100

    mail for comment notification, set markers regarding trip data, fix bug and refacto on map.js

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -20,35 +20,19 @@ function initializeAutocomplete(id, setLatAndLng) {
 }
 
 function onPlaceChanged() {
-  // if (id == 'trip_query') {
-    console.log('entre dans le place changed');
-    var place = this.getPlaces();
-    console.log(place);
-    var lat = place.geometry.location.lat();
-    var lng = place.geometry.location.lng();
-    if ($('#trip_latitude').length + $('#trip_longitude').length > 1) {
-      $('#trip_latitude').attr('value', lat);
-      $('#trip_longitude').attr('value', lng);
-    } else if ($('#experience_latitude').length + $('#experience_longitude').length > 1) {
-      $('#experience_latitude').attr('value', lat);
-      $('#experience_longitude').attr('value', lng);
-    } else if ($('#trip_comment_latitude').length + $('#trip_comment_longitude').length > 1) {
-      $('#trip_comment_latitude').attr('value', lat);
-      $('#trip_comment_longitude').attr('value', lng);
-    }
-
-    // console.log(place);  // Uncomment this line to view the full object returned by Google API.
-
-    // for (var i in place.address_components) {
-    //   var component = place.address_components[i];
-    //   for (var j in component.types) {  // Some types are ["country", "political"]
-    //     var type_input = document.getElementById(component.types[j]);
-    //     if (type_input) {
-    //       type_input.value = component.long_name;
-    //     }
-    //   }
-    // }
-  // }
+  var place = this.getPlace();
+  var lat = place.geometry.location.lat();
+  var lng = place.geometry.location.lng();
+  if ($('#trip_latitude').length + $('#trip_longitude').length > 1) {
+    $('#trip_latitude').attr('value', lat);
+    $('#trip_longitude').attr('value', lng);
+  } else if ($('#experience_latitude').length + $('#experience_longitude').length > 1) {
+    $('#experience_latitude').attr('value', lat);
+    $('#experience_longitude').attr('value', lng);
+  } else if ($('#trip_comment_latitude').length + $('#trip_comment_longitude').length > 1) {
+    $('#trip_comment_latitude').attr('value', lat);
+    $('#trip_comment_longitude').attr('value', lng);
+  }
 }
 
 if(typeof google !== "undefined") {
