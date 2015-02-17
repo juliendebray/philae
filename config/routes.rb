c48338fbf534d54f5f8fd323b764e044c796f420
florian-j <florian.joulie@gmail.com>
2015-02-17 17:48:39 +0100

    ajout d'une page avec la liste des destinations et renvoi depuis la HP et navbar

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
     end
 
     resources :story, only: [:index]
+    resources :destinations, only: [:index]
     resources :trips, only: [:update, :show, :create, :orders, :destroy] do
       resources :ask_your_friends, only: [:index]
       member do
