26589b064d56e534fec99471be2b3a705bbb36ed
florian-j <florian.joulie@gmail.com>
2015-02-21 11:56:21 -0600

    mise en ligne perou

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'json'
 require 'rest_client'
-url_json = 'https://spreadsheets.google.com/feeds/list/1E1Dqa0Xi3NV0E1t57fBEFm8udzahIa_XuZnR4gbPhQ8/od6/public/values?alt=json'
+url_json = 'https://spreadsheets.google.com/feeds/list/1dgJpdDW8j7mt_R9gzFooSEqrBjj0lspi5McC3IMJJYE/od6/public/values?alt=json'
 data_hash = JSON.parse(RestClient.get(url_json))
 data_hash['feed']['entry'].each do |exp_data|
   exp = Experience.create(
@@ -19,7 +19,7 @@ data_hash['feed']['entry'].each do |exp_data|
     description: exp_data['gsx$descriptionfrench']['$t'],
     published: true
   )
-  gen_url = "http://philae-floju.s3.amazonaws.com/photos_mexique/"
+  gen_url = "http://philae-floju.s3.amazonaws.com/photos_perou/"
   code = exp_data['gsx$code']['$t']
   (1..4).each do |i|
     complete_url = gen_url + code + "_#{i}.jpg"
