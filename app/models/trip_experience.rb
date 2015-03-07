ea2b4160a18a67bcd58486818bcb9f3431216997
Julien Debray <julien.debray.pro@gmail.com>
2015-03-07 12:26:17 +0100

    set order after validation if nil

--- app/models/trip_experience.rb
+++ app/models/trip_experience.rb
@@ -1,4 +1,15 @@
 class TripExperience < ActiveRecord::Base
   belongs_to :trip
   belongs_to :experience
+
+  after_validation :set_order, if: :set_order_needed?
+
+  private
+  def set_order_needed?
+    self.order.nil?
+  end
+
+  def set_order
+    self.order = self.trip.trip_experiences.count + 1
+  end
 end
