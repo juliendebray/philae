0069a2ba888cf7fa20ae5ed8c5974a8d1526d4de
Julien Debray <julien.debray.pro@gmail.com>
2015-03-24 15:29:53 +0100

    add active record associations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,4 @@
 class Destination < ActiveRecord::Base
+  has_many :recommended_trip_experiences
+  has_many :recommended_trips, through: :recommended_trip_experiences
 end
--- app/models/recommended_trip.rb
+++ app/models/recommended_trip.rb
@@ -1,2 +1,4 @@
 class RecommendedTrip < ActiveRecord::Base
+  has_many :recommended_trip_experiences
+  has_many :destinations, through: :recommended_trip_experiences
 end
