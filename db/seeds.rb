c0a1c0fd7fb81fd029907190ad5e33a1dbdd750c
Julien Debray <julien.debray.pro@gmail.com>
2015-03-25 14:24:46 +0100

    seed database to have lebanese experiences

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,30 +27,30 @@ end
 
 # require 'json'
 # require 'rest_client'
-# url_json = 'https://spreadsheets.google.com/feeds/list/19aquHyh7DOdJppZ4-NbT9h1oeTX0pGRZfEHZwym6BiM/od6/public/values?alt=json'
-# data_hash = JSON.parse(RestClient.get(url_json))
-# data_hash['feed']['entry'].each do |exp_data|
-#   exp = Experience.create(
-#     average_rating: exp_data['gsx$rating']['$t'].to_f,
-#     name: exp_data['gsx$nomfrench']['$t'],
-#     address: exp_data['gsx$lieufrench']['$t'],
-#     latitude: exp_data['gsx$coord']['$t'].split(", ")[0].to_f,
-#     longitude: exp_data['gsx$coord']['$t'].split(", ")[1].to_f,
-#     description: exp_data['gsx$descriptionfrench']['$t'],
-#     published: true
-#   )
-#   gen_url = "http://philae-floju.s3.amazonaws.com/photos_liban/"
-#   code = exp_data['gsx$code']['$t']
-#   (1..4).each do |i|
-#     complete_url = gen_url + code + "_#{i}.jpg"
-#     begin
-#       doc = open(complete_url)
-#     rescue OpenURI::HTTPError
-#       next
-#     end
-#     exp.experience_pictures.create(picture: complete_url)
-#   end
-# end
+url_json = 'https://spreadsheets.google.com/feeds/list/19aquHyh7DOdJppZ4-NbT9h1oeTX0pGRZfEHZwym6BiM/od6/public/values?alt=json'
+data_hash = JSON.parse(RestClient.get(url_json))
+data_hash['feed']['entry'].each do |exp_data|
+  exp = Experience.create(
+    average_rating: exp_data['gsx$rating']['$t'].to_f,
+    name: exp_data['gsx$nomfrench']['$t'],
+    address: exp_data['gsx$lieufrench']['$t'],
+    latitude: exp_data['gsx$coord']['$t'].split(", ")[0].to_f,
+    longitude: exp_data['gsx$coord']['$t'].split(", ")[1].to_f,
+    description: exp_data['gsx$descriptionfrench']['$t'],
+    published: true
+  )
+  gen_url = "http://philae-floju.s3.amazonaws.com/photos_liban/"
+  code = exp_data['gsx$code']['$t']
+  (1..4).each do |i|
+    complete_url = gen_url + code + "_#{i}.jpg"
+    begin
+      doc = open(complete_url)
+    rescue OpenURI::HTTPError
+      next
+    end
+    exp.experience_pictures.create(picture: complete_url)
+  end
+end
 
 
 
