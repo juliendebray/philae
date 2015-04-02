33e42bc498e54a8f170c62eed992eeeeb0faa096
florian-j <florian.joulie@gmail.com>
2015-04-02 14:05:15 +0200

    add experience_review to active admnin

---
+++ app/admin/experience_review.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register ExperienceReview do
+
+  permit_params :experience_id, :comment, :rating, :user_id, :created_at, :updated_at, :name, :original_date
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
