af5854e69264a0f84a4469498b3be3a9edca81f2
florian-j <florian.joulie@gmail.com>
2015-04-02 17:12:40 +0200

    exp-modal (scroll + donne ton avis) + improve experience page)

--- app/controllers/experience_reviews_controller.rb
+++ app/controllers/experience_reviews_controller.rb
@@ -12,11 +12,22 @@ class ExperienceReviewsController < ApplicationController
      experience_review.save
     if experience_review.save
       flash[:notice] = "Review has been saved"
-      redirect_to experience_path(@experience)
+      # redirect_to experience_path(@experience)
+      redirect_to :back
     else
       render :new
     end
   end
+
+  # def create_from_modal
+  #   experience_review = @experience.experience_reviews.new(experience_params)
+  #    experience_review.user = current_user
+  #    experience_review.save
+  #   if experience_review.save
+  #     flash[:notice] = "Review has been saved"
+  #     render :new
+  #   end
+  # end
 
   private
   def find_experience
@@ -24,6 +35,6 @@ class ExperienceReviewsController < ApplicationController
   end
 
   def experience_params
-    params.require(:experience_review).permit(:comment, :rating)
+    params.require(:experience_review).permit(:comment, :rating, :name, :created_at, :original_date)
   end
 end
