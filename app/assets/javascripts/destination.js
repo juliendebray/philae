dbbe022c809d9cc286e06dc2b75402677127114a
Julien Debray <julien.debray.pro@gmail.com>
2015-04-09 10:46:15 +0200

    WIP: destination view

---
+++ app/assets/javascripts/destination.js
@@ -0,0 +1,15 @@
+$(document).on('ready', function(){
+  initializeCollapseScrollDown();
+});
+
+function initializeCollapseScrollDown() {
+  initializeCollapseScrollDownWithId("collapseOverview");
+  initializeCollapseScrollDownWithId("collapseMustSee");
+  initializeCollapseScrollDownWithId("collapseRecommendedTrip");
+}
+
+function initializeCollapseScrollDownWithId(id){
+  $('#' + id).on('shown.bs.collapse', function () {
+  $('html, body').animate( { scrollTop: $(this).offset().top }, 600 );
+  });
+}
