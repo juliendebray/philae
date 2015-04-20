5fee0fba40d5fe226670f60dd2ce49ac6c8cd2e1
florian-j <florian.joulie@gmail.com>
2015-04-20 15:23:59 +0200

    test fb

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -18,17 +18,18 @@ window.fbAsyncInit = function() {
 // Publish the url on facebook wall
 function publishPostOnFbWall(fb_element_id, url) {
   $(fb_element_id).on('click', function() {
-    FB.ui(
+    FB.api(
+      'me/explorizers:design',
+      'post',
       {
-        method: 'share',
-        href: url
+        trip: "http://www.explorizers.com/trips/<%= "#{@trip.id}/#{@trip.token}" %>"
       },
-    function(response) {
-      if (response && !response.error_code) {
-        alert('Posting completed.');
-      } else {
-        alert('Error while posting.');
-      }
-    });
+      function(response) {
+        if (response && !response.error_code) {
+          alert('Posting completed.');
+        } else {
+          alert('Error while posting.');
+        }
+      });
   });
 }
