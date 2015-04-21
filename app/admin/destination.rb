7e4a258fe35798efa295212aa18a2d2013ea0c63
Julien Debray <julien.debray.pro@gmail.com>
2015-04-21 09:54:33 +0200

    add active admin for destination model

---
+++ app/admin/destination.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Destination do
+
+  permit_params :overview, :ne_lat, :ne_lng, :sw_lat, :sw_lng
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
