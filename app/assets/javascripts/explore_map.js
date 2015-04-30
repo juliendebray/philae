1c590df2ba459c280b5817998307e4d34994b52d
Julien Debray <julien.debray.pro@gmail.com>
2015-04-30 11:23:56 +0200

    add js function to change dropdown menu title and btn active state

--- app/assets/javascripts/explore_map.js
+++ app/assets/javascripts/explore_map.js
@@ -1,7 +1,19 @@
 function changeTitleOnDropDownMenu() {
-
+  $(".dropdown-menu.dropdown-titles li a").click(function(){
+    var selText = $(this).text();
+    $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+  });
 }
 
 function manageActiveClassOnClick() {
-
+  $('.category-btn').on('click', function() {
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $(this).addClass('active');
+    }
+  });
 }
+
+
+
