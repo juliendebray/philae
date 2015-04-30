ec9e9bafd92fcc43bb6fc58064c0832416fbea16
Julien Debray <julien.debray.pro@gmail.com>
2015-04-30 17:56:24 +0200

    add country_code to experience model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421080754) do
+ActiveRecord::Schema.define(version: 20150430155541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 20150421080754) do
     t.float    "fsq_rating"
     t.integer  "fsq_votes"
     t.integer  "destination_id"
+    t.string   "country_code"
   end
 
   add_index "experiences", ["category_id"], name: "index_experiences_on_category_id", using: :btree
