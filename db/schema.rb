9a8b3d272a87bc0066bfb39b1043f4403f1adff6
florian-j <florian.joulie@gmail.com>
2015-05-13 17:12:42 +0200

    add time to spend for each exp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415145850) do
+ActiveRecord::Schema.define(version: 20150513150607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20150415145850) do
     t.integer  "fsq_votes"
     t.boolean  "unesco"
     t.integer  "destination_id"
+    t.string   "timetospent"
   end
 
   add_index "experiences", ["category_id"], name: "index_experiences_on_category_id", using: :btree
