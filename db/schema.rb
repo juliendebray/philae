db07bc6e28a105cc38d7b68da08e982f550ff1c3
florian-j <florian.joulie@gmail.com>
2015-05-20 19:47:07 +0200

    added onesentence to experiences

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150520173516) do
+ActiveRecord::Schema.define(version: 20150520174244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20150520173516) do
     t.string   "timetospent"
     t.string   "wheretosleep"
     t.string   "transportation"
+    t.string   "onesentence"
   end
 
   add_index "experiences", ["category_id"], name: "index_experiences_on_category_id", using: :btree
