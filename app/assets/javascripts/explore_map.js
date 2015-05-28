8ffeee63737dc90dae843d1f53c626bd7659b55f
Julien Debray <julien.debray.pro@gmail.com>
2015-05-28 16:44:59 +0200

    add js function and modal to display experience detail

---
+++ app/assets/javascripts/explore_map.js
@@ -0,0 +1,34 @@
+function SetAndRemoveButtonActiveClassOnClick() {
+  $('a.category-btn').on('click', function() {
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $(this).addClass('active');
+    }
+  });
+  $('a.sort-by-btn').on('click', function() {
+    if ($(this).hasClass('active')) {
+      return
+    } else {
+      $(this).parents('.btn-group').find('.active').removeClass('active');
+      $(this).addClass('active');
+    }
+  });
+}
+
+function collectUserChoice() {
+  var sortBy = $('a.sort-by-btn.active').prop('id');
+  var categories = [];
+  var activeButton = $('a.category-btn.active');
+  $.each(activeButton, function(index, value) {
+    categories.push(value.id);
+  });
+  return { 'sort_by': sortBy, 'categories': categories }
+}
+
+function requestDataFromAPI(trip_id, dataToSend) {
+  var url = "/trips/" + trip_id + "/search_results"
+  $.getJSON(url, dataToSend, function( data ) {
+    console.log(data);
+  });
+}
