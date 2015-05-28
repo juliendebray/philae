44a43c7b06ae3bcc8f03cd5ae5c61564946f6aa8
Julien Debray <julien.debray.pro@gmail.com>
2015-05-28 12:02:47 +0200

    fix on home page: remove fadeout effect

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,18 +3,18 @@ $(document).on('ready', function() {
 });
 
 
-function FadeIndexonScroll(id) {
-  var divs = $(id);
-      $(window).on('scroll', function() {
-  var st = $(this).scrollTop();
-      divs.css({ 'opacity' : (1 - st/450) });
+function FadeIndexonScroll(selector) {
+  var divs = $(selector);
+  $(window).on('scroll', function() {
+    var st = $(this).scrollTop();
+    divs.css({ 'opacity' : (1 - st/450) });
   });
 
-  $(window).scroll(function() {
-  if ($(this).scrollTop()>450) {
-      $('.fader').fadeOut();
-  } else {
-      $('.fader').show();
-      }
-  });
+  // $(window).scroll(function() {
+  //   if ($(this).scrollTop()>450) {
+  //     $('.fader').fadeOut();
+  //   } else {
+  //     $('.fader').show();
+  //   }
+  // });
 }
