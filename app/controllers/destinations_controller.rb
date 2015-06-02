08a1d57ec888e732f5b23bc1d37950c1c6e14e0f
florian-j <florian.joulie@gmail.com>
2015-06-02 12:48:35 +0200

    corrected destination picture

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,6 +2,7 @@ class DestinationsController < ApplicationController
   before_action :set_destination, only: [:show, :must_see]
 
   def index
+    @destination = Destination.all
   end
 
   def show
@@ -11,6 +12,10 @@ class DestinationsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_destination
       @destination = Destination.find(params[:id])
+    end
+
+    def destination_params
+      params.require(:destination).permit(:name, destination_pictures_attributes: [:picture])
     end
 
 end
