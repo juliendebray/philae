2b05592140d3c6aba5b40bb065841d15ee27eed7
Julien Debray <julien.debray.pro@gmail.com>
2015-06-08 17:16:54 +0200

    ability to select a recommended trip with a one click button

--- app/controllers/recommended_trips_controller.rb
+++ app/controllers/recommended_trips_controller.rb
@@ -4,6 +4,10 @@ class RecommendedTripsController < ApplicationController
   def add_to_trip
     @trip = Trip.find(params[:trip_id])
     @recommended_trip = RecommendedTrip.find(params[:recommended_trip_id])
+    order = @trip.trip_experiences.count + 1
+    @recommended_trip.recommended_trip_experiences.each do |rte|
+      @trip.trip_experiences.create(recommended_trip_id: @recommended_trip.id, experience_id: rte.experience.id, order: rte.order + order)
+    end
     raise
   end
 end
