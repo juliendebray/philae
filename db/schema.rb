f9b3648e1af725e1e657b0405875f4c4a784bda0
Julien Debray <julien.debray.pro@gmail.com>
2015-06-19 11:41:38 +0200

    add campaign source for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611204501) do
+ActiveRecord::Schema.define(version: 20150619094034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -343,6 +343,7 @@ ActiveRecord::Schema.define(version: 20150611204501) do
     t.string   "token"
     t.datetime "token_expiry"
     t.boolean  "admin"
+    t.string   "from_campaign"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
